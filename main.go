da7a0a30a5dba6be6b34af2e82934456773b7a79
ben <smoilbig@126.com>
2018-03-24 22:10:00 +0800

    按行读取信息。

--- main.go
+++ main.go
@@ -2,34 +2,44 @@ package main
 
 import (
 	"fmt"
-	"net"
+	"io"
 	"log"
-	"time"
+	"net"
 )
 
-func HandleConn(conn net.Conn)  {
+func HandleConn(conn net.Conn) {
 	fmt.Println("on cline connection!")
 	conn.Write([]byte("fuck you!!!\n"))
 	conn.Write([]byte("is very good!\n"))
-	time.Sleep(time.Minute/3)
-	conn.Close()
+
+	buf := make([]byte, 1024)
+	for {
+		n, err := conn.Read(buf)
+		if err == io.EOF {
+			conn.Close()
+		}
+		fmt.Print(string(buf[:n]))
+	}
+	//conn.Write(buf)
+	//fmt.Println(string(buf[:n])) //将接受的内容都读取出来。
+
 }
 
-func main()  {
+func main() {
 
 	fmt.Println("socket server start, port 8080!!!")
-	addr:= "0.0.0.0:8080"
-	listener, err :=net.Listen("tcp", addr)
+	addr := "0.0.0.0:8080"
+	listener, err := net.Listen("tcp", addr)
 
-	if err != nil{
+	if err != nil {
 		log.Fatal(err)
 	}
 
 	defer listener.Close() //close listener
 
-	for{
+	for {
 		conn, err := listener.Accept() //conn 接收链接
-		if err !=nil{
+		if err != nil {
 			log.Fatal(err)
 		}
 
